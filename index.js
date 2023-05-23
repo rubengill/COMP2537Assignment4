a6b2cf29a98b0b2c28838a74f597a533e65e9e2d
Ruben Gill <rgill288@my.bcit.ca>
2023-05-23 11:46:09 -0700

    Step 15.a Add a difficulty level to the game. Show the levels controls to the header.

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-
-
-
-
-
-
-
-
-
-
 //Counter to keep track of number of clicks, defined outside of the function so it is not reset with every
 // "setup" call
 let clickCount = 0;
@@ -31,23 +21,86 @@ function startTimer() {
   }, 1000);
 }
 
+//Function that resets the timer 
+function resetTimer() {
+  clearInterval(timerInterval);
+  elapsedTime = 0;
+  $("#timer").text(elapsedTime);
+}
+
+//Function that shuffles the cards
+function shuffleCards() {
+  let cards = $("#game_grid").children();
+  let cardArray = $.makeArray(cards);
+
+  // Actually perform the shuffle
+  cardArray.sort(function () { return Math.random() - 0.5; });
+
+  // Empty the game grid and re-append the cards in the new order
+  $("#game_grid").empty();
+  $.each(cardArray, function (idx, itm) { $("#game_grid").append(itm); });
+}
+
 //Essentially is the function that starts the game
 //Calculate totalPairs, Run starTimer, setup, and show the game after the DOM has fully loaded to avoid any errors
 $(document).ready(function () {
-  totalPairs = $(".card").length / 2;
-  console.log("Cards: " + totalPairs);
 
   $("#start-button").on("click", function () {
-    // Show the game
-    $("#game").show();
-
-    setup();
-
-    startTimer();
-
-    //Hide start button
+    // Hide start button and difficulty buttons
     $(this).hide();
+    $("#difficulty-buttons").hide();
 
+    // Get a list of all Pokemon
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=150')  // limit set to 150 to restrict to first 150 pokemon
+      .then(response => response.json())
+      .then(data => {
+        let allPokemon = data.results;
+        let randomPokemon = [];
+
+        // Pick 3 random Pokemon
+        for (let i = 0; i < 3; i++) {
+          let randomIndex = Math.floor(Math.random() * allPokemon.length);
+          randomPokemon.push(allPokemon[randomIndex]);
+        }
+
+        // For each random Pokemon, get the Pokemon's data (including image URL)
+        Promise.all(
+          randomPokemon.map((pokemon, index) => {
+            // Return a new Promise for each fetch request
+            return fetch(pokemon.url)
+              .then(response => response.json())
+              .then(pokemonData => {
+                // Create two new cards with the Pokemon's image
+                for (let i = 0; i < 2; i++) {
+                  let card = `
+                    <div class="card">
+                      <img id="img${index * 2 + i + 1}" class="front_face" src="${pokemonData.sprites.front_default}" alt="${pokemonData.name}">
+                      <img class="back_face" src="back.webp" alt="">
+                    </div>
+                  `;
+
+                  // Append the new card to the game grid
+                  $("#game_grid").append(card);
+                }
+              });
+          })
+        ).then(() => {
+          // Now that all fetch requests have completed, we can shuffle the cards and calculate totalPairs
+
+          shuffleCards();
+
+          totalPairs = $(".card").length / 2;
+          console.log("Cards: " + totalPairs);
+          $("#remaining-pairs").text(`Remaining Pairs: ${totalPairs}`);
+
+          // Show the game
+          $("#game").show();
+
+          setup();
+
+          startTimer();
+        });
+      });
   });
 
   // Function that defines two variables, first card and second card 
@@ -134,13 +187,6 @@ $(document).ready(function () {
       }
     });
 
-    //Function that resets the timer 
-    function resetTimer() {
-      clearInterval(timerInterval);
-      elapsedTime = 0;
-      $("#timer").text(elapsedTime);
-    }
-
     $("#reset-button").on("click", function () {
       // Turn off click events on the cards
       $(".card").off("click");
@@ -151,18 +197,23 @@ $(document).ready(function () {
       secondCard = undefined;
       matchedPairs = 0;
       clickCount = 0;
+      totalPairs = 0;
       $("#click-counter").text(`Total Clicks: ${clickCount}`);
       $("#matched-pairs").text(`Matched Pairs: ${matchedPairs}`);
       $("#remaining-pairs").text(`Remaining Pairs: ${totalPairs - matchedPairs}`);
 
+      // Clear game grid
+      $("#game_grid").empty();
+
+      // Hide game
+      $("#game").hide();
+
+      // Show start button and difficulty buttons
+      $("#start-button").show();
+      $("#difficulty-buttons").show();
+
       // Reset the timer
       resetTimer();
-
-      // Restart the game
-      setup();
-
-      // Restart the timer
-      startTimer();
     });
   }
 
