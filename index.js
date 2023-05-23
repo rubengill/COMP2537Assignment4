0d3b64b03080a36799a1f0e6ab719a4b008510f1
Ruben Gill <rgill288@my.bcit.ca>
2023-05-22 19:04:59 -0700

    Step 8. fixed logic issue so first and second card reset after there is a match or no match. Added if statement to check if the second card clicked is the same as the first, if so do nothing.

--- index.js
+++ index.js
@@ -4,30 +4,34 @@ const setup = () => {
   let secondCard = undefined
   //Click event listener on all elements of class "card", remember "." notation to select classes 
   $(".card").on(("click"), function () {
+    if (firstCard && firstCard.id === $(this).find(".front_face")[0].id) {
+      return;
+    }
     $(this).toggleClass("flip");
-
+    //Sets firstCard to the first card the user clicks 
     if (!firstCard)
       firstCard = $(this).find(".front_face")[0]
     else {
+      //if firstCard is defined, set secondCard to the next card the user clicks. 
       secondCard = $(this).find(".front_face")[0]
       console.log(firstCard, secondCard);
-      if (
-        firstCard.src
-        ==
-        secondCard.src
-      ) {
+      //If they are the same, log that they match 
+      if (firstCard.src == secondCard.src) {
         console.log("match")
         $(`#${firstCard.id}`).parent().off("click")
         $(`#${secondCard.id}`).parent().off("click")
+        firstCard = undefined;
+        secondCard = undefined;
       } else {
         console.log("no match")
         setTimeout(() => {
           $(`#${firstCard.id}`).parent().toggleClass("flip")
           $(`#${secondCard.id}`).parent().toggleClass("flip")
+          firstCard = undefined;
+          secondCard = undefined;
         }, 1000)
       }
     }
   });
 }
-
 $(document).ready(setup)
